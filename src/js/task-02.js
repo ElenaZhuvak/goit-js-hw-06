389631204085823a63c783e62d51c483e315e1e0
ElenaZhuvak <elenkazh@gmail.com>
2022-10-12 21:00:24 +0100

    Task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('ul#ingredients');
+console.log(listIngredients);
+
+const itemsEl = ingredients.map(ingredient => {
+  const item = document.createElement('li');
+  item.classList.add('item');
+  item.textContent = ingredient;
+
+  return item;
+});
+
+console.log(itemsEl);
+
+listIngredients.append(...itemsEl);
