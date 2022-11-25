1f999d900ca1d38da05167e39e549664d2b99178
ElenaZhuvak <elenkazh@gmail.com>
2022-11-25 22:03:28 +0000

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,14 @@
-
-
-
 const counterValue = 0;
+
+const valueRef = document.querySelector('#value');
+console.log(valueRef);
+
+const decrementRef = document.querySelector('#counter button[data-action="decrement"]');
+concole.log(decrementRef);
+
+
+// const saveBtn = document.querySelector('button[data-action="save"]');
+// console.log(saveBtn.dataset.action); // "save"
+
+// const closeBtn = document.querySelector('button[data-action="close"]');
+// console.log(closeBtn.dataset.action); // "close"
