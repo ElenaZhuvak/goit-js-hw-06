f95672b5ea61a5426083aaef097bea93d801c306
ElenaZhuvak <elenkazh@gmail.com>
2022-11-27 19:00:13 +0000

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,14 +1,36 @@
-const counterValue = 0;
-
+// Ссылки на объекты
 const valueRef = document.querySelector('#value');
+const btnDecrementRef = document.querySelector('[data-action="decrement"]');
+const btnIncrementRef = document.querySelector('[data-action="increment"]');
+
 console.log(valueRef);
+console.log(btnDecrementRef);
+console.log(btnIncrementRef);
+// ==============
 
-const decrementRef = document.querySelector('#counter button[data-action="decrement"]');
-concole.log(decrementRef);
+// Вешаем слушатель события
+const counterValue = 0;
+btnDecrementRef.addEventListener('click', function () {
+    // console.log('Клик по кнопке -');
+    counterValue -= 1;
+});
+
+btnIncrementRef.addEventListener('click', function () {
+    // console.log('Клик по кнопке +');
+    counterValue += 1;
+});
 
 
-// const saveBtn = document.querySelector('button[data-action="save"]');
-// console.log(saveBtn.dataset.action); // "save"
 
-// const closeBtn = document.querySelector('button[data-action="close"]');
-// console.log(closeBtn.dataset.action); // "close"
+
+
+
+
+
+
+
+
+//? 
+// !
+//*  
+// TODO 
