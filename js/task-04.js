fae185b28b225912c8d6f68e4b8b1e3da2199ae4
ElenaZhuvak <elenkazh@gmail.com>
2022-11-29 21:31:44 +0000

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,25 +1,31 @@
 // Ссылки на объекты
-const valueRef = document.querySelector('#value');
 const btnDecrementRef = document.querySelector('[data-action="decrement"]');
 const btnIncrementRef = document.querySelector('[data-action="increment"]');
+const valueRef = document.querySelector('#value');
 
-console.log(valueRef);
 console.log(btnDecrementRef);
 console.log(btnIncrementRef);
+console.log(valueRef);
 // ==============
+//* так как переменная меняет свое значение используем let
+let counterValue = 0; 
 
 // Вешаем слушатель события
-const counterValue = 0;
+
 btnDecrementRef.addEventListener('click', function () {
     // console.log('Клик по кнопке -');
     counterValue -= 1;
+    // valueRef.innerHTML = counterValue;
+    valueRef.textContent = counterValue;
 });
 
 btnIncrementRef.addEventListener('click', function () {
     // console.log('Клик по кнопке +');
     counterValue += 1;
+    // valueRef.innerHTML = counterValue;
+    valueRef.textContent = counterValue;
 });
-
+  // работает через innerHTML и через textContent
 
 
 
