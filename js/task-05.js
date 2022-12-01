bef826fe8fd9733295c0c737bd9008872314d053
ElenaZhuvak <elenkazh@gmail.com>
2022-12-01 21:21:37 +0000

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,18 @@
+//* стандартное объявление ссылок на объекты
+const input = document.querySelector('input#name-input');
+const span = document.querySelector('span#name-output');
+
+console.log(input);
+console.log(span);
+
+//* Объявление ссылок через один объект refs (при вызове использовать refs.input или refs.span)
+// const refs = {
+//     input: document.querySelector('input#name-input'),
+//     span: document.querySelector('span#name-output')
+// };
+
+// вешаем слушатель событий
+input.addEventListener('input', function (event) {
+    console.log(event.currentTarget.value);
+    span.textContent = event.currentTarget.value || (span.textContent = 'Anonymous'); 
+});
